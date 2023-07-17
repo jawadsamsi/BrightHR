251282fcda8cb833dbad3fd20e422e029815f2eb
Jawad Samsi <jawadsamsi@hotmail.com>
2023-07-17 03:40:45 +0100

    Created a new test file, CheckoutTests. Added a first method, Scan_EmptyItemScan_ReturnException which is a fail case.

---
+++ CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    /*
+     * Tests for Checkout
+     * Red - Green - Refractor
+     */
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void Scan_EmptyItemScan_ReturnException()
+        {
+            // Arrange
+            ICheckout checkout = new Checkout();
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => checkout.Scan(""));
+        }
+    }
+}
